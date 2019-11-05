e8764a54fbd07e7a2d8ba0436330fa0fb97baf58
aslllllo <49677332+aslllllo@users.noreply.github.com>
2019-11-05 12:45:24 +0800

    pipeline test

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-      <p> Hello Jenkins </p>
+      <p> Hello Jenkins Pipeline</p>
         <a
           className="App-link"
           href="https://reactjs.org"
